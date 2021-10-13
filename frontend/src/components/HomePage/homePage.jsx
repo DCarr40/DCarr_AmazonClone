3007a5148e60ede32a685f0bff8903169ae0f363
DCarr40 <davidwcarruthers@yahoo.com>
2021-10-13 12:11:25 -0400

    create react app for frontend and added git ignores for packages then added Home functional component

---
+++ frontend/src/components/HomePage/homePage.jsx
@@ -0,0 +1,426 @@
+import React from "react";
+import "./style.css";
+
+function HomePage() {
+  return (
+    <div class="grid-container">
+      <header class="row">
+        <div>
+          <a class="brand" href="index.html">
+            DCarrAmazonClone
+          </a>
+        </div>
+
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+
+      <main>
+        <div class="row center">
+          <div class="card">
+            <a href="product.html">
+              {/* image size should be 680 x 830 px*/}
+              <img
+                class="medium"
+                src="../../../image/captainAmericaFigure"
+                alt="product"
+              />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Captain America Figure</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+              <div class="price">$65</div>
+            </div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              {/* image size should be 680 x 830 px*/}
+              <img
+                class="medium"
+                src="./images/captainAmericaFigure.jpeg"
+                alt="product"
+              />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Captain America Figure</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+              <div class="price">$65</div>
+            </div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              {/* image size should be 680 x 830 px*/}
+              <img
+                class="medium"
+                src="./images/captainAmericaFigure.jpeg"
+                alt="product"
+              />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Captain America Figure</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+              <div class="price">$65</div>
+            </div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              {/* image size should be 680 x 830 px*/}
+              <img
+                class="medium"
+                src="./images/captainAmericaFigure.jpeg"
+                alt="product"
+              />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Captain America Figure</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+              <div class="price">$65</div>
+            </div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              {/* image size should be 680 x 830 px*/}
+              <img
+                class="medium"
+                src="./images/captainAmericaFigure.jpeg"
+                alt="product"
+              />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Captain America Figure</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+              <div class="price">$65</div>
+            </div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              {/* image size should be 680 x 830 px*/}
+              <img
+                class="medium"
+                src="./images/captainAmericaFigure.jpeg"
+                alt="product"
+              />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Captain America Figure</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+              <div class="price">$65</div>
+            </div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              {/* image size should be 680 x 830 px*/}
+              <img
+                class="medium"
+                src="./images/captainAmericaFigure.jpeg"
+                alt="product"
+              />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Captain America Figure</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+              <div class="price">$65</div>
+            </div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              {/* image size should be 680 x 830 px*/}
+              <img
+                class="medium"
+                src="./images/captainAmericaFigure.jpeg"
+                alt="product"
+              />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Captain America Figure</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+              <div class="price">$65</div>
+            </div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              {/* image size should be 680 x 830 px*/}
+              <img
+                class="medium"
+                src="./images/captainAmericaFigure.jpeg"
+                alt="product"
+              />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Captain America Figure</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+              <div class="price">$65</div>
+            </div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              {/* image size should be 680 x 830 px*/}
+              <img
+                class="medium"
+                src="./images/captainAmericaFigure.jpeg"
+                alt="product"
+              />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Captain America Figure</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+              <div class="price">$65</div>
+            </div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              {/* image size should be 680 x 830 px*/}
+              <img
+                class="medium"
+                src="./images/captainAmericaFigure.jpeg"
+                alt="product"
+              />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Captain America Figure</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+              <div class="price">$65</div>
+            </div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              {/* image size should be 680 x 830 px*/}
+              <img
+                class="medium"
+                src="./images/captainAmericaFigure.jpeg"
+                alt="product"
+              />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Captain America Figure</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+              <div class="price">$65</div>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer class="row center">All Rights Reserved</footer>
+    </div>
+  );
+}
+
+export default HomePage;
