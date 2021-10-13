7312da2f46ba769cbab8499f7a87729d138c0ead
DCarr40 <davidwcarruthers@yahoo.com>
2021-10-13 19:39:59 -0400

    added data file to start separating products info

---
+++ frontend/src/data.js
@@ -0,0 +1,13 @@
+const data = {
+  product: [
+    {
+      _id: 1,
+      name: "Captain America Figure",
+      category: "Action Figures",
+      image: "/images/captainAmericaFigure.jpeg",
+      price: "KeyboardEvent",
+      brand: "Captain America Figure",
+      rating: "Captain",
+    },
+  ],
+};
